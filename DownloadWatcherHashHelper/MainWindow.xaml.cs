4c06d930aa67f1c6c1b1774fa4e0291685e1d709
mdberryh <opencoder@opencoder.us>
2016-11-25 12:06:06 -0500

    improved the view to allow future applets
    The design currently only has the hasher, but we can add a duplicate
    finder and a monitoring utility.

--- DownloadWatcherHashHelper/MainWindow.xaml.cs
+++ DownloadWatcherHashHelper/MainWindow.xaml.cs
@@ -41,8 +41,7 @@ namespace DownloadWatcherHashHelper
             InitializeComponent();
 
 
-            FirstHash.Children.Add(new FileHashingControl());
-            FirstHash.Children.Add(new FileHashingControl());
+            AppContent.Children.Add(new FileHasherControlPage());
 
             // fileCreatedWindow = new FileCreatedDetected();
             // fileCreatedWindow.Hide();
@@ -162,74 +161,5 @@ namespace DownloadWatcherHashHelper
             this.Close();
         }
 
-        private void BtnCompare_Click(object sender, RoutedEventArgs e)
-        {
-            //popup window for file to compare against.
-            //var popup = new MainWindow(this);
-            //popup.ShowDialog();
-
-            var firstFile = (FileHashingControl)FirstHash.Children[0];
-            var SecondFile = (FileHashingControl)FirstHash.Children[1];
-            
-            if(firstFile.ValidHash() && SecondFile.ValidHash())
-            {
-                if(firstFile.GetHash() == SecondFile.GetHash())
-                {
-                    MessageBox.Show("File matches! File should be fine to run");
-                }
-                else
-                {
-                    MessageBox.Show("NOT A MATCH...DO NOT RUN IF COMPARING THE HASH!!");
-                }
-            }            
-        }
-
-        private void btnFileBrowse_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-
-            // Get the selected file name and display in a TextBox 
-            if (openFileDialog.ShowDialog() == true)
-            {
-                // Open document 
-                string filename = openFileDialog.FileName;
-            //    txtBxFileName.Text = filename;
-            //    txtBxFileSize.Text = GetFileSize(filename).ToString();
-                //hash the file
-                //txtBxFileHash.Text = GetFileHash(filename);
-             //   cmbHashType.SelectedItem = cmbHashType.Items[1]; //sha256
-            }
-        }
-
-        private string GetFileHash(string path)
-        {
-            var fhasher = new FileHasher();
-            return fhasher.getFileHashSha256(path);
-
-        }
-        private static long GetFileSize(string filename)
-        {
-            FileInfo fi = new FileInfo(filename);
-            return fi.Length;
-        }
-        private void cmbHashType_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var fhasher = new FileHasher();
-
-            var item = (ComboBoxItem)e.AddedItems[0];
-            string text = item.Content.ToString();
-            if (text == "MD5")
-            {
-                //set to md5 hash
-               // txtBxFileHash.Text = fhasher.getFileHashMd5(txtBxFileName.Text);
-            }
-            if (text == "Sha256")
-            {
-                //set to md5 hash
-               // txtBxFileHash.Text = fhasher.getFileHashSha256(txtBxFileName.Text);
-            }
-
-        }
     }
 }
